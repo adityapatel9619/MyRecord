b2ce0988cf0a7413e82b82a67e504f590a0124f6
payalpawar <payal.pawar.qls@gmail.com>
2022-09-10 19:26:17 +0530

    Menu Font Change

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -44,42 +44,45 @@ namespace dskMyStudents
             this.aCTIONSToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 31);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(600, 25);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // sTUDENTToolStripMenuItem
             // 
-            this.sTUDENTToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sTUDENTToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.sTUDENTToolStripMenuItem.Name = "sTUDENTToolStripMenuItem";
-            this.sTUDENTToolStripMenuItem.Size = new System.Drawing.Size(56, 27);
+            this.sTUDENTToolStripMenuItem.Size = new System.Drawing.Size(42, 21);
             this.sTUDENTToolStripMenuItem.Text = "FILE";
             // 
             // aCTIONSToolStripMenuItem
             // 
             this.aCTIONSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.nEWToolStripMenuItem});
-            this.aCTIONSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aCTIONSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.aCTIONSToolStripMenuItem.Name = "aCTIONSToolStripMenuItem";
-            this.aCTIONSToolStripMenuItem.Size = new System.Drawing.Size(101, 27);
+            this.aCTIONSToolStripMenuItem.Size = new System.Drawing.Size(76, 21);
             this.aCTIONSToolStripMenuItem.Text = "STUDENT";
             // 
             // nEWToolStripMenuItem
             // 
+            this.nEWToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nEWToolStripMenuItem.Name = "nEWToolStripMenuItem";
-            this.nEWToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.nEWToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.nEWToolStripMenuItem.Text = " ADD NEW";
             this.nEWToolStripMenuItem.Click += new System.EventHandler(this.nEWToolStripMenuItem_Click);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "MY STUDENTS";
